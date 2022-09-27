5cf249687c4b412332adbe1b7917c342018b738a
Saskia Lablans <k21009051@kcl.ac.uk>
2022-09-27 12:56:28 +0100

    Complete assignment

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,18 @@
 """Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    # Convert every element in the list to a string
+    for element in items:
+        index = items.index(element)
+        items[index] = str(element)
+    
+    # Generate a unique set of the elements to be counted
+    set_items = set(items)
+
+    for element in set_items:
+        count = items.count(element)
+        frequencies[element] = count
+
     return frequencies
